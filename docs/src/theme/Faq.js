51ef2b1b031145f2f192004a248f0b9651da474f
zepatrik <zepatrik@users.noreply.github.com>
2021-03-03 11:28:20 +0100

    faq fix second approach

--- docs/src/theme/Faq.js
+++ docs/src/theme/Faq.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import cn from 'classnames'
 import styles from './faq.module.css'
 
-const Question = ({ children, tags }) => (
-  <div className={cn(tags.map((tag) => styles[tag]))}>{children}</div>
-)
+const Question = ({children, tags, selectedTags}) =>
+  // check if any element in tags is included in selectedTags
+  tags.reduce((acc, curr) => selectedTags.find(t => t === curr) || acc, false) ? (
+    <div className={cn(tags.map((tag) => styles[tag]))}>{children}</div>
+  ) : null
 
-const TagButton = ({ tag, isSelected, children, toggleSelected }) => (
+const TagButton = ({tag, isSelected, children, toggleSelected}) => (
   <li
     className={cn(
-      { [styles.selected]: isSelected },
+      {[styles.selected]: isSelected},
       styles[tag],
       'pills',
       'pills__item',
-      { 'styles.pills__item--active': isSelected }
+      {'styles.pills__item--active': isSelected}
     )}
     onClick={toggleSelected}
   >
@@ -21,7 +23,7 @@ const TagButton = ({ tag, isSelected, children, toggleSelected }) => (
   </li>
 )
 
-const Faq = ({ tags, switchofftags }) => {
+const Faq = ({tags, switchofftags}) => {
   const [selectedTags, setSelectedTags] = useState(
     tags.filter((t) => !switchofftags.includes(t))
   )
@@ -53,8 +55,22 @@ const Faq = ({ tags, switchofftags }) => {
           #{tag}
         </TagButton>
       ))}
+      {/* from here on generate by script */}
+      <Question selectedTags={selectedTags} tags={["asdf", "asdf"]}>
+        #kratos #database <br />
+        <p>
+          <strong>Q</strong>: We want to seperate our customers and employees, so we
+          store them in different databases. But we would like to have them use the
+          same login dialog for our portal. How can I achieve that with Ory Kratos?
+        </p>
+        <p>
+          <strong>A</strong>: You can deploy Ory Kratos two times, and use the same
+          login/registration endpoints if you like. You may need to tell you
+          login/registration ui which Kratos it is supposed to talk to.
+        </p>
+      </Question>
     </>
   )
 }
 
-export { Faq, Question }
+export {Faq, Question}
